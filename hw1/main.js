41bbe54b68691b54bfe540ea7f6b55f335fb120e
SobokarNadia <n.sobokar@gmail.com>
2022-10-31 21:43:01 +0100

    hw

---
+++ hw1/main.js
@@ -0,0 +1,109 @@
+let a = 'hello';
+
+console.log(a);
+alert(a);
+document.write(a);
+
+let b = 'owu';
+
+console.log(b);
+alert(b);
+document.write(b);
+
+let c = 'com';
+
+console.log(c);
+alert(c);
+document.write(c);
+
+let d ='ua';
+
+console.log(d);
+alert(d);
+document.write(d);
+
+let e = 1;
+
+console.log(e);
+alert(e);
+document.write(e);
+
+let f = 10;
+
+console.log(f);
+alert(f);
+document.write(f);
+
+let g = (-999);
+
+console.log(g);
+alert(g);
+document.write(g);
+
+let h = 123;
+
+console.log(h);
+alert(h);
+document.write(h);
+
+let i = 3.14;
+
+console.log(i);
+alert(i);
+document.write(i);
+
+let j = 2.7;
+
+console.log(j);
+alert(j);
+document.write(j);
+
+let k = 16;
+
+console.log(k);
+alert(k);
+document.write(k);
+
+let l = true;
+
+console.log(l);
+alert(l);
+document.write(l);
+
+let m = false;
+
+console.log(m);
+alert(m);
+document.write(m);
+
+
+
+let firstName = 'Nadiia';
+let middleName = 'Volodymyrivna';
+let lastName = 'Sobokar';
+let person = `${lastName} ${firstName} ${middleName}`;
+
+
+
+let an = 100;
+let bn = '100';
+let cn  = true;
+
+console.log(an, typeof an);
+console.log(bn, typeof bn);
+console.log(cn, typeof cn);
+
+
+
+let name = prompt("Ім'я:");
+let anotherName = prompt('По батькові:');
+let age = prompt('Вік:');
+
+console.log(`Ім'я - ${name} По батькові -  ${anotherName} Вік - ${age}`);
+
+
+
+
+
+
+
