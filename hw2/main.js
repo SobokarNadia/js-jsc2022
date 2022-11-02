f761632713204b62d369cc5e17566223282f1342
SobokarNadia <n.sobokar@gmail.com>
2022-11-02 21:22:30 +0100

    hw2

---
+++ hw2/main.js
@@ -0,0 +1,122 @@
+let arr = [1, 2, true, false, 'smth', 6, 'seven', false, 9, 'ten'];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+
+let book1 = {
+    title: 'Three Comrades',
+    pageCount: 496,
+    genre: 'novel'
+};
+let book2 = {
+    title: 'Arch of Triumph',
+    pageCount: 464,
+    genre: 'novel'
+};
+let book3 = {
+    title: 'Heaven Has No Favorites',
+    pageCount: 320,
+    genre: 'novel'
+};
+
+let bookFirst = {
+    title: 'Three Comrades',
+    pageCount: 496,
+    genre: 'novel',
+    authors: [{
+        name: 'Erich Maria Remarque',
+        birthYear: 1898
+    }]
+};
+let bookSecond = {
+    title: 'Arch of Triumph',
+    pageCount: 464,
+    genre: 'novel',
+    authors: [{
+        name: 'Erich Maria Remarque',
+        birthYear: 1898
+    }]
+};
+let bookThird = {
+    title: 'Heaven Has No Favorites',
+    pageCount: 320,
+    genre: 'novel',
+    authors: [{
+        name: 'Erich Maria Remarque',
+        birthYear: 1898
+    }]
+};
+
+
+let users = [
+    {
+        name: 'vasya',
+        username: 'vasya',
+        password: '123e'
+    },
+    {
+        name: 'masha',
+        username: 'masha',
+        password: '123e'
+    },
+    {
+        name: 'rita',
+        username: 'rita',
+        password: '123e'
+    },
+    {
+        name: 'misa',
+        username: 'misha',
+        password: '123e'
+    },
+    {
+        name: 'sasha',
+        username: 'sasha',
+        password: '123e'
+    },
+    {
+        name: 'pavel',
+        username: 'pavel',
+        password: '123e'
+    },
+    {
+        name: 'katya',
+        username: 'katya',
+        password: '123e'
+    },
+    {
+        name: 'dasha',
+        username: 'dasha',
+        password: '123e'
+    },
+    {
+        name: 'natasha',
+        username: 'natasha',
+        password: '123e'
+    },
+    {
+        name: 'vova',
+        username: 'vova',
+        password: '123e'
+    },
+];
+
+console.log(users[1].password);
+console.log(users[0].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+
